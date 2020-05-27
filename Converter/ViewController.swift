be9c8f9d3501cb2a02a995166dcb103769e5e64f
Rommer <rommer.chu@triosstudent.com>
2020-05-26 21:52:57 -0400

    converter

--- Converter/ViewController.swift
+++ Converter/ViewController.swift
@@ -5,16 +5,43 @@
 //  Created by Rommer Chu on 2020-05-19.
 //  Copyright © 2020 Rommer Chu. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let kgs: Double = 2.20462
+    let lbs: Double = 0.45359237
+    
+    //1 kgs = 2.20462 lbs
+    //1 lbs = 0.45359237 kgs
+    
+    
+    @IBOutlet weak var kilogram: UITextField!
+    
+    //kilogram = pounds: UITExtField * 2.20462
+    //return(kilogram)
 
+    @IBOutlet weak var pounds: UITextField!
+    
+    //pounds = kilogram: UITextField * 0.45359237
+    //return(pounds)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnButton(_ sender: UIButton) {
+        
+        //func result (kilogram:Double, pounds:Double){
+            //kilogram = pounds: UITExtField * 2.20462
+            //pounds = kilogram: UITextField * 0.45359237
+            //return(kilogram, pounds)
+            
+        //}
+        
+    }
+    
 }
 
